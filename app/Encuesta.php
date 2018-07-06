41cf63ebe68f780ed36a0c5183389a2c5094d22e
Nestor Paucar <nespaucar@gmail.com>
2018-07-06 11:51:34 -0500

    Update Encuesta.php

--- app/Encuesta.php
+++ app/Encuesta.php
@@ -5,6 +5,9 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+use App\Direccion;
+use Illuminate\Support\Facades\DB;
+
 class Encuesta extends Model
 {
     use SoftDeletes;
@@ -26,27 +29,51 @@ class Encuesta extends Model
         return $this->hasMany('App\Pregunta');
     }
 
+    public function direcciones()
+    {
+        return $this->hasMany('App\Direccion');
+    }
+
     /**
      * Método para listar
      * @param  model $query modelo
      * @param  string $nombre  nombre
      * @return sql        sql
      */
-    public function scopelistar($query, $nombre, $tipoencuesta_id)
+    public function scopelistar($query, $nombre, $tipoencuesta_id, $facultad_id, $escuela_id, $especialidad_id)
     {
-        return $query->where(function($subquery) use($nombre)
-		            {
-		            	if (!is_null($nombre)) {
-		            		$subquery->where('nombre', 'LIKE', '%'.$nombre.'%');
-		            	}
-		            })
-        			->where(function($subquery) use($tipoencuesta_id)
-		            {
-		            	if (!is_null($tipoencuesta_id)) {
-		            		$subquery->where('tipoencuesta_id', '=', $tipoencuesta_id);
-		            	}
-		            })
-        			->orderBy('tipoencuesta_id', 'ASC')
-        			->orderBy('nombre', 'ASC');
+        $c = false;
+        if($facultad_id != '' || $escuela_id != '' || $especialidad_id != '') {
+            $direccion = Direccion::select('encuesta_id')->where('facultad_id', '=', $facultad_id)->where('escuela_id', '=', $escuela_id)->where('especialidad_id', '=', $especialidad_id)->get();
+
+            if (count($direccion) != 0) {
+                $c = true;
+            } 
+        } else {
+            $c = true;
+        }
+
+        return $query->where(function($subquery) use($c, $direccion, $facultad_id, $escuela_id, $especialidad_id)
+        {
+            if ($c) {
+                foreach ($direccion as $row) {
+                    $subquery->orWhere('id', '=', $row['encuesta_id']);
+                }
+            } else {
+                $subquery->where('nombre', '=', '----------');
+            }
+        })
+        ->where(function($subquery) use($c, $nombre, $tipoencuesta_id){
+            if($c){
+                $subquery->where('nombre', 'LIKE', '%'.$nombre.'%');
+                if (!is_null($tipoencuesta_id)) {
+                    $subquery->where('tipoencuesta_id', '=', $tipoencuesta_id);
+                } 
+            } else {
+                $subquery->where('nombre', '=', '----------');
+            }
+        })
+		->orderBy('tipoencuesta_id', 'ASC')
+		->orderBy('nombre', 'ASC');
     }
 }
