57acd6a49ed96c70804bcbc0bbd887e8d7aedd6e
Nestor Paucar <nespaucar@gmail.com>
2018-07-12 01:29:04 -0500

    Add files via upload

---
+++ resources/views/app/alumnoencuesta/respuestasencuesta.blade.php
@@ -0,0 +1,28 @@
+<?php 
+use App\Respuesta;
+use App\Alternativa;
+?>
+<div class="form-group border">
+	<?php $i = 1; ?>
+	@foreach($preguntas as $pregunta)
+		{!! '<b><u><span class=\'text-info\'>'.$i. '. ' .$pregunta->nombre.'<span></u></b><br>' !!}
+		<?php 
+			$alternativas = Alternativa::select('id', 'nombre')->where('pregunta_id', '=', $pregunta->id)->get();
+			foreach ($alternativas as $alternativa) {
+				$respuesta = Respuesta::select('id')->where('alternativa_id', '=', $alternativa->id)->get();
+				if(count($respuesta) > 0) {
+					echo '<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>RESPUESTA: </b> <b>' . $alternativa->nombre . '</b><br><br>';
+				}
+			}
+		?>
+		<?php $i++; ?>
+	@endforeach
+</div>
+<div class="form-group text-center">
+	{!! Form::button('Cerrar', array('class' => 'btn btn-warning btn-sm', 'id' => 'btnCerrar', 'onclick' => 'cerrarModal();')) !!}
+</div>
+<script type="text/javascript">
+$(document).ready(function() {
+	configurarAnchoModal('400');
+}); 
+</script>
