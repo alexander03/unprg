edc46111495b6cebb730860de4a6e7c3ee6b7a78
Nestor Paucar <nespaucar@gmail.com>
2018-07-16 11:59:35 -0500

    Update list.blade.php

--- resources/views/app/alumno/list.blade.php
+++ resources/views/app/alumno/list.blade.php
@@ -1,6 +1,18 @@
 @if(count($lista) == 0)
 <h3 class="text-warning">No se encontraron resultados.</h3>
 @else
+<script>
+	function cambiarsituacion(idalumno) {
+		var situacion = $('#situacion' + idalumno).val();
+		$.ajax({
+			url: 'alumno/cambiarsituacion?idalumno=' + idalumno + '&situacion=' + situacion,
+            headers: {'X-CSRF-TOKEN': '{{ csrf_token() }}' },
+            type: 'GET',
+		}).fail(function(){
+			alert('Ocurrió un error');
+		});
+	}
+</script>
 {!! $paginacion or '' !!}
 <table id="example1" class="table table-bordered table-striped table-condensed table-hover">
 
@@ -23,7 +35,7 @@
 			<td>{{ $value->nombres.' '.$value->apellidopaterno.' '.$value->apellidomaterno  }}</td>		
 			<td>{{ $value->escuela->nombre or  '-'  }}</td>
 			<td>{{ $value->especialidad->nombre or '-' }}</td>
-			<td>{{ $value->situacion or '-' }}</td>
+			<td>{!! Form::select('situacion' . $value->id, $cboSituacion, $value->situacion, array('class' => 'form-control input-xs', 'id' => 'situacion' . $value->id, 'onchange' => 'cambiarsituacion('. $value->id .');')) !!}</td>
 			<td>{!! Form::button('<div class="glyphicon glyphicon-lock"></div> Restablecer contraseña', array('onclick' => 'modal (\''.URL::route($ruta["password"], array($value->id, 'SI')).'\', \''.$titulo_password.'\', this);', 'class' => 'btn btn-xs btn-secondary')) !!}</td>
 			<td>{!! Form::button('<div class="glyphicon glyphicon-pencil"></div> Editar', array('onclick' => 'modal (\''.URL::route($ruta["edit"], array($value->id, 'listar'=>'SI')).'\', \''.$titulo_modificar.'\', this);', 'class' => 'btn btn-xs btn-warning')) !!}</td>
 			<td>{!! Form::button('<div class="glyphicon glyphicon-remove"></div> Eliminar', array('onclick' => 'modal (\''.URL::route($ruta["delete"], array($value->id, 'SI')).'\', \''.$titulo_eliminar.'\', this);', 'class' => 'btn btn-xs btn-danger')) !!}</td>
